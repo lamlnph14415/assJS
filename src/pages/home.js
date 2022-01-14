ae4eb4c38ad3423be8dbc8e98b01970b4cc24757
ngoc lam <lamlnph14415@fpt.edu.vn>
2022-01-14 16:31:56 +0700

    capnhat

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,31 @@
+import News from "../components/news";
+
 const HomePage = {
     render() {
-        return `Home Page in object`;
+        return /* html */`
+                <!-- banner -->
+            <div class="mt-4">
+                <img class="w-full h-auto" src="./img//banner.jpg" alt="">
+            </div>
+            <!-- nd -->
+            <div>
+                ${News.render()}
+            </div>
+        `;
     },
 };
 export default HomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
