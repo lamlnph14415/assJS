e5741cf8c1574db0829cb921b3ada2cc04968842
ngoc lam <lamlnph14415@fpt.edu.vn>
2022-01-19 16:49:29 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,34 +1,24 @@
 import Navigo from "navigo";
-import DetailNewsPage from "./pages/detailNews";
+import Dashboard from "./pages/admin/dashboard";
+import AdminNewsPage from "./pages/admin/news";
+import CTDT from "./pages/ctdt";
+// import AboutPage from "./pages/about";
 import HomePage from "./pages/home";
+import NewsPage from "./pages/news";
 
 const router = new Navigo("/", { linksSelector: "a" });
+
 const print = (content) => {
-    document.getElementById("app").innerHTML = content;
+    document.querySelector("#app").innerHTML = content.render();
 };
 
 router.on({
-    "/": () => {
-        print(HomePage.render());
-    },
-    "/tuyensinh": () => {
-        print("Trang tuyển sinh");
-    },
-    "/chuongtringdaotao": () => {
-        print("Chương trình đào tạo");
-    },
-    "/goctuyensinh": () => {
-        print("Góc tuyển sinh");
-    },
-    "/tuyendung": () => {
-        print("Tuyển dụng");
-    },
-    "/news/:id": (value) => {
-        console.log(value.data.id);
-        print(DetailNewsPage.render(value.data.id));
-    },
+    "/": () => print(HomePage),
+    "/ctdt": () => print(CTDT),
+    "/news": () => print(NewsPage),
+    "/admin/dashboard": () => print(Dashboard),
+    "/admin/news": () => print(AdminNewsPage),
+    "/admin/ctdt": () => print(Dashboard),
+
 });
-
-router.notFound(() => print("Not Found Page"));
-
 router.resolve();
